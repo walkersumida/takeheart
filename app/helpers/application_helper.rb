b04cab0d7e59bb2007e9d1832345b2359aba0917
WalkerSumida <walkersumida@gmail.com>
2016-05-07 22:56:15 +0900

    Add Material Design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def get_img_path(img_name)
+    "/#{IMAGES_DIR}/#{img_name}"
+  end
 end
--- app/helpers/maxims_helper.rb
+++ app/helpers/maxims_helper.rb
@@ -1,2 +1,5 @@
 module MaximsHelper
+  def output_img(menu_img_name, option = {})
+    image_tag get_img_path(menu_img_name), option
+  end
 end
